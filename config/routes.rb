c597223358cb4aa3c7976ab007e35b53c66d9653
Vincent Turnier <vincentturnier@gmail.com>
2019-05-25 15:50:53 -0400

    added origin rout using / pointing to countries/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'countries/index'
+  get '/' => 'countries/index'
   get 'greeter/hello'
   get 'greeter/bye'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
